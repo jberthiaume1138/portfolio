008210859ffc40e70f79c852634bd879f5260144
jberthiaume1138 <joenyc23@gmail.com>
2016-05-11 02:48:31 -0400

    changes

--- js/app.js
+++ js/app.js
@@ -9,16 +9,19 @@
 'use strict'
 
 $(document).ready(function() {
-
+	// $(window).resize(function() {
+	// 	var windowHeight = $(document).height();
+	// 	$('#main').css({'height': windowHeight});
+	// });
 });
 
-//smooth scroll
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-    return false;
-});
+// //smooth scroll
+// $('a').click(function(){
+//     $('html, body').animate({
+//         scrollTop: $( $.attr(this, 'href') ).offset().top
+//     }, 500);
+//     return false;
+// });
 
 // // smooth scroll with cache
 // var $root = $('html, body');
@@ -29,14 +32,14 @@ $('a').click(function(){
 //     return false;
 // });
 
-// smooth scroll and updates the URL
-// var $root = $('html, body');
-// $('a').click(function() {
-//     var href = $.attr(this, 'href');
-//     $root.animate({
-//         scrollTop: $(href).offset().top
-//     }, 500, function () {
-//         window.location.hash = href;
-//     });
-//     return false;
-// });
+//smooth scroll and updates the URL
+var $root = $('html, body');
+$('a').click(function() {
+    var href = $.attr(this, 'href');
+    $root.animate({
+        scrollTop: $(href).offset().top
+    }, 500, function () {
+        window.location.hash = href;
+    });
+    return false;
+});
