21009016cf91b9c0ef1cca0938c8f4dfc2d99cac
jberthiaume1138 <joenyc23@gmail.com>
2016-06-03 02:46:25 -0400

    link fine tuning

--- js/app.js
+++ js/app.js
@@ -3,20 +3,13 @@
 $(document).ready(function() {
     $('#nav-header').hide();
 
-        $(window).on('scroll',function() {
-        // window scrolled event fired
-        // determine if position is past bottom of #banner
-
-        var verticalPosition = window.pageYOffset;
+    $(window).on('scroll',function() {
+        // window scrolled fired determine if position is past bottom of #banner
 
         var bannerHeight = $('#banner').height();   // this is fixed in CSS
-
         var bannerTop = $('#banner').offset().top;
 
-        var bannerBottom = bannerTop + bannerHeight;
-
-        
-        // console.log('Scroll pos: ' + $(window).scrollTop() + ' ' + bannerTop + ' ' + bannerBottom);    
+        var bannerBottom = bannerTop + bannerHeight;      
 
         if($(window).scrollTop() > bannerBottom) {
             $('#nav-header').show();
@@ -27,10 +20,6 @@ $(document).ready(function() {
     });
 });
 
-
-
-
-
 // //smooth scroll
 // $('a').click(function(){
 //     $('html, body').animate({
@@ -39,14 +28,6 @@ $(document).ready(function() {
 //     return false;
 // });
 
-// // smooth scroll with cache
-// var $root = $('html, body');
-// $('a').click(function() {
-//     $root.animate({
-//         scrollTop: $( $.attr(this, 'href') ).offset().top
-//     }, 500);
-//     return false;
-// });
 
 //smooth scroll and updates the URL
 var $root = $('html, body');
@@ -59,15 +40,3 @@ $('a').click(function() {
     });
     return false;
 });
-
-// //smooth scroll and updates the URL
-// var $root = $('html, body');
-// $('a').click(function() {
-//     var href = $.attr(this, 'href');
-//     $root.animate({
-//         scrollTop: $(href).offset().top
-//     }, 500, function () {
-//         window.location.hash = href;
-//     });
-//     return false;
-// });
